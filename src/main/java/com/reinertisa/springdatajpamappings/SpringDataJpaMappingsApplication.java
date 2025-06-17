532c259d14c04851a44706d5de709ea65f034b20
Isa Reinert <reinert.isa@gmail.com>
2025-06-16 23:25:36 -0700

    feat(api): initialize project

---
+++ src/main/java/com/reinertisa/springdatajpamappings/SpringDataJpaMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.reinertisa.springdatajpamappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaMappingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reinertisa/springdatajpamappings/SpringDataJpaMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinertisa.springdatajpamappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaMappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
