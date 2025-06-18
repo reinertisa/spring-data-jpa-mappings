307d307868fa43a938a6c8701f5abb18dbb556fa
Isa Reinert <reinert.isa@gmail.com>
2025-06-18 12:14:42 -0700

    feat(api): update address request

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/request/AddressRequest.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/request/AddressRequest.java
@@ -1,11 +1,13 @@
 package com.reinertisa.springdatajpamappings.one_to_one_uni.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = false)
 public class AddressRequest {
     @NotBlank(message = "City name is required.")
     private String city;
