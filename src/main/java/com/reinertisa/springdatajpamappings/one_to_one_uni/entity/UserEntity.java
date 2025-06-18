e94f666796bede35dfd1cea9b73436ea79820ba6
Isa Reinert <reinert.isa@gmail.com>
2025-06-18 00:46:20 -0700

    feat(api): update user entity property columns

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/entity/UserEntity.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/entity/UserEntity.java
@@ -18,8 +18,11 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @OneToOne(
