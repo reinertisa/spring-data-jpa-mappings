b8c5a4a017903c9da5a7fbf459f5b41b9d11cc9a
Isa Reinert <reinert.isa@gmail.com>
2025-06-26 01:06:43 -0700

    feat(api): create student controller

---
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_many_uni/controller/StudentController.java
@@ -0,0 +1,12 @@
+package com.reinertisa.springdatajpamappings.one_to_many_uni.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+}
