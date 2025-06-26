b6201612ea38b12e56324f7e94d006bdf1e713cf
Isa Reinert <reinert.isa@gmail.com>
2025-06-25 23:37:10 -0700

    feat(api): create laptop entity

---
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_many_uni/entity/LaptopEntity.java
@@ -0,0 +1,27 @@
+package com.reinertisa.springdatajpamappings.one_to_many_uni.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "laptops")
+public class LaptopEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand", nullable = false)
+    private String brand;
+
+    @Column(name = "model", nullable = false)
+    private String model;
+}
