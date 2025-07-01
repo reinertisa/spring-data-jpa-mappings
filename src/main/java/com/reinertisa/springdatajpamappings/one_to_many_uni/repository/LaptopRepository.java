ac0c13134fcd110e309ce236761eebd417d07434
Isa Reinert <reinert.isa@gmail.com>
2025-06-30 23:08:59 -0700

    fix(api): update laptop repository

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_many_uni/repository/LaptopRepository.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_many_uni/repository/LaptopRepository.java
@@ -2,8 +2,14 @@ package com.reinertisa.springdatajpamappings.one_to_many_uni.repository;
 
 import com.reinertisa.springdatajpamappings.one_to_many_uni.entity.LaptopEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LaptopRepository extends JpaRepository<LaptopEntity, Long> {
+    @Query("SELECT l from LaptopEntity l WHERE l.brand LIKE %:key% OR l.model LIKE %:key%")
+    List<LaptopEntity> findByKeyword(@Param("key") String keyword);
 }
