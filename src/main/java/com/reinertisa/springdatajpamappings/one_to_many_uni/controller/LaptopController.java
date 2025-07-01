58cfffaacdda07ddf89b8da2d17818ebe49d722e
Isa Reinert <reinert.isa@gmail.com>
2025-06-30 23:09:13 -0700

    fix(api): update laptop controller

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_many_uni/controller/LaptopController.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_many_uni/controller/LaptopController.java
@@ -1,6 +1,8 @@
 package com.reinertisa.springdatajpamappings.one_to_many_uni.controller;
 
 import com.reinertisa.springdatajpamappings.one_to_many_uni.dto.LaptopDto;
+import com.reinertisa.springdatajpamappings.one_to_many_uni.exception.ResourceNotFoundException;
+import com.reinertisa.springdatajpamappings.one_to_many_uni.request.LaptopRequest;
 import com.reinertisa.springdatajpamappings.one_to_many_uni.service.LaptopService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -8,10 +10,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,6 +26,37 @@ public class LaptopController {
             @PageableDefault(size = 20, page = 0, sort = "brand")Pageable pageable) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(laptopService.getAllLaptops(pageable));
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LaptopDto> getLaptopById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(laptopService.getLaptopById(id));
+        } catch (ResourceNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LaptopDto> updateLaptop(@RequestBody LaptopRequest laptopRequest, @PathVariable Long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(laptopService.updateLaptop(id, laptopRequest));
+        } catch (ResourceNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<List<LaptopDto>> filter(@RequestParam("keyword") String keyword) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(laptopService.filterLaptops(keyword));
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         }
