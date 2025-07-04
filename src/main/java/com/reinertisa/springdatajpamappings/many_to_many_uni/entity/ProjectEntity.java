b5ed25f67e333730bffb5f543aecc785ef4ca974
Isa Reinert <reinert.isa@gmail.com>
2025-07-03 23:58:07 -0700

    feat(api): create project entity

---
+++ src/main/java/com/reinertisa/springdatajpamappings/many_to_many_uni/entity/ProjectEntity.java
@@ -0,0 +1,23 @@
+package com.reinertisa.springdatajpamappings.many_to_many_uni.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "projects")
+public class ProjectEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "project_name", nullable = false)
+    private String projectNama;
+}
