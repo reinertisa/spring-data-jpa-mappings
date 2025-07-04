3992bc4be127b9fcdc8114b97a6a76230aa7f9c2
Isa Reinert <reinert.isa@gmail.com>
2025-07-03 23:58:00 -0700

    feat(api): create employee entity

---
+++ src/main/java/com/reinertisa/springdatajpamappings/many_to_many_uni/entity/EmployeeEntity.java
@@ -0,0 +1,42 @@
+package com.reinertisa.springdatajpamappings.many_to_many_uni.entity;
+
+import com.reinertisa.springdatajpamappings.many_to_many_uni.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @ManyToMany(
+            targetEntity = ProjectEntity.class,
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH}
+    )
+    @JoinTable(
+            name = "employee_project",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "project_id")
+    )
+    private Set<ProjectEntity> projects = new HashSet<>();
+}
