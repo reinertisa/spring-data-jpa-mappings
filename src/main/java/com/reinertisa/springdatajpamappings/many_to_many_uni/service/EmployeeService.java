5eb8f4086ac8c2b500c4484929d74cdbf38d0915
Isa Reinert <reinert.isa@gmail.com>
2025-07-08 23:47:29 -0700

    feat(api): create employee service

---
+++ src/main/java/com/reinertisa/springdatajpamappings/many_to_many_uni/service/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.reinertisa.springdatajpamappings.many_to_many_uni.service;
+
+import com.reinertisa.springdatajpamappings.many_to_many_uni.dto.EmployeeDto;
+import com.reinertisa.springdatajpamappings.many_to_many_uni.exception.ResourceNotFoundException;
+import com.reinertisa.springdatajpamappings.many_to_many_uni.request.EmployeeRequest;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    Page<EmployeeDto> getAllEmployees(Pageable pageable);
+
+    EmployeeDto getEmployeeById(Long Id) throws ResourceNotFoundException;
+
+    EmployeeDto createEmployee(@Valid EmployeeRequest employeeRequest);
+
+    EmployeeDto updateEmployee(Long id, EmployeeRequest employeeRequest) throws ResourceNotFoundException;
+
+    void deleteStudent(Long id) throws ResourceNotFoundException;
+
+    List<EmployeeDto> filterByName(String name);
+}
