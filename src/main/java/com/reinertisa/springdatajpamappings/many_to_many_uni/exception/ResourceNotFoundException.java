e08a261a2da3540a83b0fa5225399ea335727264
Isa Reinert <reinert.isa@gmail.com>
2025-07-08 23:47:21 -0700

    feat(api): create resource not found exception

---
+++ src/main/java/com/reinertisa/springdatajpamappings/many_to_many_uni/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.reinertisa.springdatajpamappings.many_to_many_uni.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Long id) {
+        super(message + " " + id);
+    }
+}
