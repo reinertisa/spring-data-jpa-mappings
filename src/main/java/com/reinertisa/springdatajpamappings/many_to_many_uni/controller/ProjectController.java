29e6efd82c5248886c84bf477e03d5814fb89402
Isa Reinert <reinert.isa@gmail.com>
2025-07-12 22:23:59 -0700

    feat(api): create project controller

---
+++ src/main/java/com/reinertisa/springdatajpamappings/many_to_many_uni/controller/ProjectController.java
@@ -0,0 +1,31 @@
+package com.reinertisa.springdatajpamappings.many_to_many_uni.controller;
+
+import com.reinertisa.springdatajpamappings.many_to_many_uni.dto.ProjectDto;
+import com.reinertisa.springdatajpamappings.many_to_many_uni.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/projects")
+public class ProjectController {
+    private final ProjectService projectService;
+
+    @GetMapping("")
+    public ResponseEntity<Page<ProjectDto>> getAllProjects(
+            @PageableDefault(size = 20, page = 0, sort = "projectName") Pageable pageable) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(projectService.getAllProjects(pageable));
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+    }
+}
