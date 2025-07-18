43351a1e00ce858649e4ca6502f12fd12f563ad6
Isa Reinert <reinert.isa@gmail.com>
2025-07-17 23:46:54 -0700

    feat(api): add update project api

--- src/main/java/com/reinertisa/springdatajpamappings/many_to_many_uni/controller/ProjectController.java
+++ src/main/java/com/reinertisa/springdatajpamappings/many_to_many_uni/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.reinertisa.springdatajpamappings.many_to_many_uni.controller;
 
 import com.reinertisa.springdatajpamappings.many_to_many_uni.dto.ProjectDto;
 import com.reinertisa.springdatajpamappings.many_to_many_uni.exception.ResourceNotFoundException;
+import com.reinertisa.springdatajpamappings.many_to_many_uni.request.ProjectRequest;
 import com.reinertisa.springdatajpamappings.many_to_many_uni.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -9,10 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
@@ -35,6 +33,17 @@ public class ProjectController {
     public ResponseEntity<ProjectDto> getProjectById(@PathVariable Long id) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(projectService.getProjectById(id));
+        } catch (ResourceNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProjectDto> updateProject(@RequestBody ProjectRequest projectRequest, @PathVariable Long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(projectService.updateProject(id, projectRequest));
         } catch (ResourceNotFoundException ex) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
         } catch (Exception ex) {
