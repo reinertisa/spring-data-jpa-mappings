a79b694f4b5b24500d37ab92b2d780de275865d1
Isa Reinert <reinert.isa@gmail.com>
2025-07-19 23:57:29 -0700

    feat(api): add update employee api

--- src/main/java/com/reinertisa/springdatajpamappings/many_to_many_uni/controller/EmployeeController.java
+++ src/main/java/com/reinertisa/springdatajpamappings/many_to_many_uni/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.reinertisa.springdatajpamappings.many_to_many_uni.controller;
 
 import com.reinertisa.springdatajpamappings.many_to_many_uni.dto.EmployeeDto;
+import com.reinertisa.springdatajpamappings.many_to_many_uni.exception.ResourceNotFoundException;
 import com.reinertisa.springdatajpamappings.many_to_many_uni.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -9,6 +10,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -24,6 +26,17 @@ public class EmployeeController {
             @PageableDefault(size = 20, page = 0, sort = "name") Pageable pageable) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(employeeService.getAllEmployees(pageable));
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(employeeService.getEmployeeById(id));
+        } catch (ResourceNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         }
