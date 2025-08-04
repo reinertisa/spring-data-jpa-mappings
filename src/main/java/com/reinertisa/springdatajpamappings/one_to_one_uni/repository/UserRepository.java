f6580a982c982462d3397705a609efe2f27cd2ae
Isa Reinert <reinert.isa@gmail.com>
2025-08-04 15:23:54 -0700

    feat(api): update user repository

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/repository/UserRepository.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.reinertisa.springdatajpamappings.one_to_one_uni.repository;
 
+import com.reinertisa.springdatajpamappings.one_to_one_uni.entity.AddressEntity;
 import com.reinertisa.springdatajpamappings.one_to_one_uni.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,10 +8,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     @Query("SELECT u FROM UserEntity u WHERE u.firstName LIKE %:name% OR u.lastName LIKE %:name%")
     List<UserEntity> findByName(@Param("name") String name);
+
+    Optional<UserEntity> findByAddressEntity(AddressEntity address);
 }
