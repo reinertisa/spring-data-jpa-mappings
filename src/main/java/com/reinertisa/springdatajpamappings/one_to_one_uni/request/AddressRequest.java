1119bc7c45e50245dbeb8d49032afb032535be28
Isa Reinert <reinert.isa@gmail.com>
2025-08-04 15:23:25 -0700

    feat(api): update zipCode validations

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/request/AddressRequest.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/request/AddressRequest.java
@@ -1,7 +1,10 @@
 package com.reinertisa.springdatajpamappings.one_to_one_uni.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,6 +18,8 @@ public class AddressRequest {
     private String state;
     @NotBlank(message = "Country name is require.")
     private String country;
-    @NotBlank(message = "Zip code is required.")
+    @NotNull(message = "Zip code is required.")
+    @Min(value = 10000, message = "Min value is 10000.")
+    @Max(value = 99999, message = "Max value is 99999.")
     private Integer zipCode;
 }
