63f8197e5bb6a62275bccc10c2a6b47be01d26ac
Isa Reinert <reinert.isa@gmail.com>
2025-08-05 23:53:21 -0700

    feat(api): create license entity

---
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/entity/LicenseEntity.java
@@ -0,0 +1,33 @@
+package com.reinertisa.springdatajpamappings.one_to_one_bi.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "licenses")
+public class LicenseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "license_number")
+    private String licenseNumber;
+    @Column(name = "state", nullable = false)
+    private String state;
+
+    @JsonBackReference
+    @OneToOne(
+            targetEntity = DriverEntity.class,
+            mappedBy = "license",
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
+    )
+    private LicenseEntity licenseEntity;
+}
