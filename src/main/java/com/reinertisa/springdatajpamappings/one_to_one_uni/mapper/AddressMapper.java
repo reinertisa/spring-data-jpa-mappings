768064b333f2508b4f753cbd26a03841fcb62f90
Isa Reinert <reinert.isa@gmail.com>
2025-08-06 01:07:34 -0700

    fix(api): check null pointer for address mapper

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/mapper/AddressMapper.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/mapper/AddressMapper.java
@@ -4,12 +4,16 @@ import com.reinertisa.springdatajpamappings.one_to_one_uni.dto.AddressDto;
 import com.reinertisa.springdatajpamappings.one_to_one_uni.entity.AddressEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.function.Function;
 
 @Component
 public class AddressMapper implements Function<AddressEntity, AddressDto> {
     @Override
     public AddressDto apply(AddressEntity addressEntity) {
+
+        Objects.requireNonNull(addressEntity, "AddressEntity must not be null");
+
         return AddressDto.builder()
                 .id(addressEntity.getId())
                 .city(addressEntity.getCity())
