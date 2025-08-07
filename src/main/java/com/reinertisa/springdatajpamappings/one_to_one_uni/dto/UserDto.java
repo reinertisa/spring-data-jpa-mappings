063a4b610e44f258c10a8e95f1d5db24ad71b84b
Isa Reinert <reinert.isa@gmail.com>
2025-08-06 18:51:36 -0700

    fix(api): add no arg constructor annotation to user dto

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/dto/UserDto.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_uni/dto/UserDto.java
@@ -3,10 +3,12 @@ package com.reinertisa.springdatajpamappings.one_to_one_uni.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private Long id;
     private String firstName;
