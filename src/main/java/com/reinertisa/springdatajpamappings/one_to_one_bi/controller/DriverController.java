f56e8aa5083e812a605e108de4d8f66f938b9724
Isa Reinert <reinert.isa@gmail.com>
2025-08-10 00:25:16 -0700

    feat(api): add create license api

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/controller/DriverController.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/controller/DriverController.java
@@ -2,17 +2,16 @@ package com.reinertisa.springdatajpamappings.one_to_one_bi.controller;
 
 import com.reinertisa.springdatajpamappings.one_to_one_bi.dto.DriverDto;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.exception.ResourceNotFoundException;
+import com.reinertisa.springdatajpamappings.one_to_one_bi.request.DriverRequest;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.service.DriverService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
@@ -35,6 +34,17 @@ public class DriverController {
     public ResponseEntity<DriverDto> getDriverById(@PathVariable Long id) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(driverService.getDriverById(id));
+        } catch (ResourceNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<DriverDto> createDriver(@RequestBody @Valid DriverRequest driverRequest) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(driverService.createDriver(driverRequest));
         } catch (ResourceNotFoundException ex) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
         } catch (Exception ex) {
--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/service/DriverServiceImpl.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/service/DriverServiceImpl.java
@@ -2,11 +2,14 @@ package com.reinertisa.springdatajpamappings.one_to_one_bi.service;
 
 import com.reinertisa.springdatajpamappings.one_to_one_bi.dto.DriverDto;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.entity.DriverEntity;
+import com.reinertisa.springdatajpamappings.one_to_one_bi.entity.LicenseEntity;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.exception.ResourceNotFoundException;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.mapper.DriverMapper;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.repository.DriverRepository;
+import com.reinertisa.springdatajpamappings.one_to_one_bi.repository.LicenseRepository;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.request.DriverRequest;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +25,7 @@ import java.util.Objects;
 public class DriverServiceImpl implements DriverService {
     private final DriverRepository driverRepository;
     private final DriverMapper driverMapper;
+    private final LicenseRepository licenseRepository;
 
     @Override
     public Page<DriverDto> getAllDrivers(Pageable pageable) {
@@ -37,8 +42,37 @@ public class DriverServiceImpl implements DriverService {
     }
 
     @Override
-    public DriverDto createDriver(DriverRequest driverRequest) {
-        return null;
+    public DriverDto createDriver(@Valid DriverRequest driverRequest) {
+        DriverEntity driver = DriverEntity.builder()
+                .fullName(driverRequest.getFullName())
+                .build();
+
+        if (driverRequest.getLicenseId() != null) {
+            LicenseEntity existingLicense = licenseRepository.findById(driverRequest.getLicenseId())
+                    .orElseThrow(() -> new ResourceNotFoundException("License not found for ID: " + driverRequest.getLicenseId()));
+
+            if (existingLicense.getDriver() != null) {
+                throw new RuntimeException("License is already used by another driver.");
+            }
+        } else if (driverRequest.getLicense() != null) {
+            Optional<LicenseEntity> existingLicenseOpt = licenseRepository.findByLicenseNumberAndState(driverRequest.getLicense().getLicenseNumber(), driverRequest.getLicense().getState());
+
+            if (existingLicenseOpt.isPresent()) {
+                LicenseEntity existingLicense = existingLicenseOpt.get();
+                if (existingLicense.getDriver() != null) {
+                    throw new RuntimeException("License is already used by another driver.");
+                }
+                driver.setLicense(existingLicense);
+            } else {
+                LicenseEntity licenseEntity = LicenseEntity.builder()
+                        .licenseNumber(driverRequest.getLicense().getLicenseNumber())
+                        .state(driverRequest.getLicense().getState())
+                        .build();
+                driver.setLicense(licenseEntity);
+            }
+        }
+
+        return driverMapper.apply(driverRepository.save(driver));
     }
 
     @Override
