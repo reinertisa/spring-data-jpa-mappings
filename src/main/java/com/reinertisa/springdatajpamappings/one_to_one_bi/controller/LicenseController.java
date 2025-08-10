ddbc14239e73fc8a433ad9b6fe4a4f126472a75b
Isa Reinert <reinert.isa@gmail.com>
2025-08-10 00:48:15 -0700

    feat(api): add create license api

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/controller/LicenseController.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/controller/LicenseController.java
@@ -1,18 +1,18 @@
 package com.reinertisa.springdatajpamappings.one_to_one_bi.controller;
 
+import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.dto.LicenseDto;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.exception.ResourceNotFoundException;
+import com.reinertisa.springdatajpamappings.one_to_one_bi.request.LicenseRequest;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.service.LicenseService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
@@ -38,6 +38,15 @@ public class LicenseController {
             return ResponseEntity.status(HttpStatus.OK).body(licenseService.getLicenseById(id));
         } catch (ResourceNotFoundException ex) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<LicenseDto> createLicense(@RequestBody @Valid LicenseRequest licenseRequest) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(licenseService.createLicense(licenseRequest));
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         }
--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/service/LicenseServiceImpl.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/service/LicenseServiceImpl.java
@@ -7,6 +7,7 @@ import com.reinertisa.springdatajpamappings.one_to_one_bi.mapper.LicenseMapper;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.repository.LicenseRepository;
 import com.reinertisa.springdatajpamappings.one_to_one_bi.request.LicenseRequest;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -38,8 +40,22 @@ public class LicenseServiceImpl implements LicenseService {
     }
 
     @Override
-    public LicenseDto createLicense(LicenseRequest licenseRequest) {
-        return null;
+    public LicenseDto createLicense(@Valid LicenseRequest licenseRequest) {
+        Optional<LicenseEntity> existingLicense = licenseRepository.findByLicenseNumberAndState(licenseRequest.getLicenseNumber(), licenseRequest.getState());
+
+        // check for duplicates before saving
+        existingLicense.ifPresent(existing -> {
+            throw new ResourceNotFoundException("License with number" + existing.getLicenseNumber() +
+                    " and state " + existing.getState() + " already. exists");
+        });
+
+        // Build and save new LicenseEntity
+        LicenseEntity license = LicenseEntity.builder()
+                .licenseNumber(licenseRequest.getLicenseNumber())
+                .state(licenseRequest.getState())
+                .build();
+
+        return licenseMapper.apply(licenseRepository.save(license));
     }
 
     @Override
