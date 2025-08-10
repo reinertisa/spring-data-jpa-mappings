356b620b6cdc18eb8900f9666b2427d581cda5a0
Isa Reinert <reinert.isa@gmail.com>
2025-08-10 00:26:21 -0700

    feat(api): add findByLicenseNumberAndState query to license repository

--- src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/repository/LicenseRepository.java
+++ src/main/java/com/reinertisa/springdatajpamappings/one_to_one_bi/repository/LicenseRepository.java
@@ -4,6 +4,10 @@ import com.reinertisa.springdatajpamappings.one_to_one_bi.entity.LicenseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LicenseRepository extends JpaRepository<LicenseEntity, Long> {
+
+    Optional<LicenseEntity> findByLicenseNumberAndState(String licenceNumber, String State);
 }
